b14cfabaa482f6a0aa63a834b663ffd32a6a14d8
dzorlu <deniz@monahq.com>
2018-03-07 22:49:54 -0500

    densenet estimator

--- models/densenet.py
+++ models/densenet.py
@@ -8,28 +8,31 @@ class Encoder(
     collections.namedtuple("Encoder", ("graph", "output"))):
   pass
 
+class Decoder(
+    collections.namedtuple("Decoder", ("graph", "output"))):
+  pass
 
-class BaseModel(object):
+
+class DenseNet(object):
   def __init__(self,
                growth_rate,
+               dropout_keep_prob,
+               number_classes,
                is_training=True,
-               global_pool=False,
-               reuse=None,
-               num_classes=None,
-               spatial_squeeze=True):
+               reuse=None,):
     self.growth_rate = growth_rate
     self.reuse = reuse
     self.is_training = is_training
-    self.global_pool = global_pool
-    self.num_classes = num_classes
-    self.spatial_squeeze = spatial_squeeze
+    self.dropout_keep_prob = dropout_keep_prob
+    self.number_classes = number_classes
+    #
+    self.encoder = None
+    self.decoder = None
 
   def encode(self,
-            image,
-            scope,
-            num_units,
-            bottleneck_number_feature_maps=None,
-            dropout_keep_prob=0.2):
+             features,
+             num_units,
+             bottleneck_number_feature_maps=None):
     """
     Args:
     	inputs: image.
@@ -39,12 +42,10 @@ class BaseModel(object):
       dropout_rate: dropout rate to apply for each conv unit in training.
       num_units: Array of number of units in each block.
     """
-    graph = tf.Graph()
     rate = self.growth_rate
     bottleneck_maps = bottleneck_number_feature_maps
-    inputs = image
-    with tf.variable_scope(scope, 'densenet', [inputs], reuse=self.reuse) as sc:
-      net = inputs
+    with tf.variable_scope('encoder', 'densenet', [features], reuse=self.reuse):
+      net = features
       initial_nb_layers = self.growth_rate * 2
       with tf.variable_scope("conv1"):
         net = tf.layers.conv2d(net,
@@ -52,9 +53,9 @@ class BaseModel(object):
                                use_bias=False,
                                kernel_size=7,
                                padding='same',
-                               strides=2)
-      with tf.variable_scope("pool1"):
-        net = tf.layers.max_pooling2d(net, [3, 3], strides=2, padding='same')
+                               strides=1)
+      # with tf.variable_scope("pool1"):
+      #   net = tf.layers.max_pooling2d(net, [3, 3], strides=2, padding='same')
       # consecutive denseblocks each followed by a transition layer.
       for bn, num_units_in_block in enumerate(num_units):
         with tf.variable_scope("block_{}".format(bn + 1), values=[net]):
@@ -64,7 +65,7 @@ class BaseModel(object):
                                                          is_training=self.is_training,
                                                          growth_rate=rate,
                                                          bottleneck_number_feature_maps=bottleneck_maps,
-                                                         dropout_keep_prob=dropout_keep_prob)
+                                                         dropout_keep_prob=self.dropout_keep_prob)
 
               net = tf.concat(axis=3, values=[net, output])
           # the last layer does not have a transition layer
@@ -74,29 +75,11 @@ class BaseModel(object):
             tf.add_to_collection('skip_connections', net)
             # downsample
             net = densenet_utils.add_transition_down_layer(net, is_training=self.is_training)
-      # if self.global_pool:
-      #   # Global average pooling.
-      #   net = tf.reduce_mean(net, [1, 2], name='pool5', keep_dims=True)
-      # if self.num_classes is not None:
-      #   with tf.variable_scope('logits'):
-      #     net = tf.layers.conv2d(net,
-      #                            self.num_classes,
-      #                            kernel_size=1,
-      #                            kernel_regularizer=None,
-      #                            activation=tf.identity)
-      #   if self.spatial_squeeze:
-      #     net = tf.squeeze(net, [1, 2], name='SpatialSqueeze')
-      # # Convert end_points_collection into a dictionary of end_points.
-      # if self.num_classes is not None:
-      #   with tf.variable_scope('softmax'):
-      #     net = tf.nn.softmax(net)
-      return Encoder(graph=net.graph, output=net)
+      self.encoder = Encoder(graph=net.graph, output=net)
 
   def decode(self,
-             encoder,
-             rate,
-             num_units,
-             dropout_keep_prob=0.2):
+             num_units):
+    encoder = self.encoder
     graph = encoder.graph
     with graph.as_default():
       net = encoder.output
@@ -114,18 +97,38 @@ class BaseModel(object):
           for un, unit in enumerate(range(num_units_in_block)):
             with tf.variable_scope("unit_{}".format(un + 1), values=[net]):
               output = densenet_utils.stack_blocks_dense(net,
-                                                         growth_rate=rate,
-                                                         dropout_keep_prob=dropout_keep_prob)
+                                                         growth_rate=self.growth_rate,
+                                                         dropout_keep_prob=self.dropout_keep_prob)
               # unlike downsampling path, do not concatenate the input layer within the dense block
               if un > 0:
                 net = tf.concat(axis=3, values=[net, output])
               else:
                 net = output
-        with tf.variable_scope('logits'):
-          net = tf.layers.conv2d(net,
-                                 self.num_classes,
-                                 kernel_size=1,
-                                 activation=tf.identity)
-      return net
+      with tf.variable_scope('logits'):
+        net = tf.layers.conv2d(net,
+                               self.number_classes,
+                               kernel_size=1,
+                               activation=tf.identity)
+    return Decoder(graph=net.graph, output=net)
+
+  def build_graph(self, image):
+    k = 12
+    num_encoder_units = [4, 5, 7, 10, 12]
+    bottleneck_number_feature_maps = k * 4
+    self.encode(features=image,
+                num_units=num_encoder_units,
+                bottleneck_number_feature_maps=bottleneck_number_feature_maps)
+    num_decoder_units = [12, 10, 7, 5]
+    self.decode(num_units=num_decoder_units)
+
+def main(unused_argv):
+  pass
+
+
+if __name__ == "__main__":
+  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.app.run()
+
+
 
 
---
+++ training/train_model.py
@@ -0,0 +1,125 @@
+import tensorflow as tf
+import argparse
+import sys
+
+sys.path.append('/Users/denizzorlu/seglib/')
+
+
+from models.densenet import DenseNet
+from data.data_generator import generate_data
+
+
+FLAGS = tf.flags.FLAGS
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch_size', default=100, type=int, help='batch size')
+parser.add_argument('--train_steps', default=1000, type=int,
+                    help='number of training steps')
+
+# Learning hyperaparmeters
+_BASE_LR = 0.1
+_LR_SCHEDULE = [  # (LR multiplier, epoch to start)
+    (1.0 / 6, 0), (2.0 / 6, 1), (3.0 / 6, 2), (4.0 / 6, 3), (5.0 / 6, 4),
+    (1.0, 5), (0.1, 30), (0.01, 60), (0.001, 80), (0.0001, 90)
+]
+
+
+def learning_rate_schedule(current_epoch):
+  """Handles linear scaling rule, gradual warmup, and LR decay."""
+  scaled_lr = _BASE_LR * (FLAGS.train_batch_size / 256.0)
+
+  decay_rate = scaled_lr
+  for mult, start_epoch in _LR_SCHEDULE:
+    decay_rate = tf.where(current_epoch < start_epoch, decay_rate,
+                          scaled_lr * mult)
+
+  return decay_rate
+
+
+def model_fn(features, labels, mode, params):
+	"""Model function for DenseNet classifier.
+	
+	Args:
+	  features: inputs.
+	  labels: one hot encoded classes
+	  mode: one of tf.estimator.ModeKeys.{TRAIN, INFER, EVAL}
+	  params: a parameter dictionary with the following keys: 
+	
+	Returns:
+	  ModelFnOps for Estimator API.
+	"""
+	number_classes = params.get('number_classes')
+	growth_rate = params.get('growth_rate')
+	dropout_keep_prob = params.get('dropout_keep_prob')
+	encoder_num_units = params.get('encoder_num_units')
+	decoder_num_units = params.get('decoder_num_units')
+	bottleneck_number_feature_maps = params.get('bottleneck_number_feature_maps')
+
+	densenet = DenseNet(growth_rate=growth_rate,
+											dropout_keep_prob=dropout_keep_prob,
+											number_classes=number_classes,
+											is_training=(mode == tf.estimator.ModeKeys.TRAIN))
+	densenet.encode(features=features,
+									num_units=encoder_num_units,
+									bottleneck_number_feature_maps=bottleneck_number_feature_maps)
+	logits = densenet.decode(decoder_num_units)
+	predictions = tf.argmax(logits, axis=1)
+	if mode == tf.estimator.ModeKeys.PREDICT:
+		predictions = {
+			'class_ids': predictions,
+			'probabilities': tf.nn.softmax(logits),
+			'logits': logits,
+		}
+		return tf.estimator.EstimatorSpec(mode, predictions=predictions)
+
+	# Add the loss.
+	# Calculate loss, which includes softmax cross entropy and L2 regularization.
+	# wraps the softmax_with_entropy fn. adds it to loss collection
+	tf.losses.softmax_cross_entropy(
+		logits=logits, onehot_labels=labels)
+	# include the regulization losses in the loss collection.
+	loss = tf.losses.get_total_loss()
+
+	if mode == tf.estimator.ModeKeys.EVAL:
+		return tf.estimator.EstimatorSpec(
+			mode=mode,
+			eval_metric_ops={"accuracy": tf.metrics.accuracy(labels, predictions)})
+
+	assert mode == tf.estimator.ModeKeys.TRAIN
+	global_step = tf.train.get_global_step()
+	current_epoch = (
+		tf.cast(global_step, tf.float32) / params["batches_per_epoch"])
+	learning_rate = learning_rate_schedule(current_epoch)
+	optimizer = tf.train.AdagradOptimizer(learning_rate=learning_rate)
+	train_op = optimizer.minimize(loss, global_step=global_step)
+	return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
+
+
+def main(argv):
+	args = parser.parse_args(argv[1:])
+	classifier = tf.estimator.Estimator(
+		model_fn=model_fn,
+		params={
+			'number_classes': 2,
+			'growth_rate': 12,
+			'dropout_keep_prob': 0.2,
+			'encoder_num_units': [4,5,7,10,12],
+			'decoder_num_units': [12, 10, 7, 5],
+			'bottleneck_number_feature_maps': 48
+		}
+	)
+
+	# Train the Model.
+	classifier.train(
+		input_fn=lambda: generate_data(args.batch_size),
+		steps=args.train_steps)
+
+	# Evaluate the model.
+	eval_result = classifier.evaluate(
+		input_fn=lambda: generate_data(args.batch_size))
+
+	print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+if __name__ == '__main__':
+	tf.logging.set_verbosity(tf.logging.INFO)
+	tf.app.run(main)
+
